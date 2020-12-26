0475a44a694bc226227bb2e08eb37ced52918caa
Obote <obotedenis256@gmail.com>
2020-12-26 22:27:10 +0300

    display results and auto scroll

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 
 const form = document.querySelector('.quiz-form');
+ 
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -16,5 +18,7 @@ form.addEventListener('submit', e => {
             score += 25; 
         }
     });
-    console.log(score);
+    scrollTo(0,0);
+    result.querySelector('span').textContent = score;
+    result.classList.remove('d-none'); 
 });
