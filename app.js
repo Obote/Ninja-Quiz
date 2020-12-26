6379d9b6c20125ace3c30f591ffde1fc18461b4a
Obote <obotedenis256@gmail.com>
2020-12-26 22:47:29 +0300

    animating the result

--- app.js
+++ app.js
@@ -20,5 +20,15 @@ form.addEventListener('submit', e => {
     });
     scrollTo(0,0);
     result.querySelector('span').textContent = score;
-    result.classList.remove('d-none'); 
-});
+    result.classList.remove('d-none');
+    
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = score;
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        } 
+    }, 50);
+}); 
